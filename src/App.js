22d0b65e8d3317d83699877a84dd05cefc6e41e3
Amanda Hotchkiss <amandapandah@yahoo.com>
2021-09-27 20:55:51 -0400

    added search engine
    added search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -7,6 +8,10 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Amanda Hotchkiss</footer>
     </div>
   );
 }
